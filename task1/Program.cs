b4aad605f4046c332e83a039b220d05392b8c56e
Zeradeil <wandark@ya.ru>
2023-04-15 20:50:09 +0700

    решено 2

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,4 +4,33 @@
 // 1, -7, 567, 89, 223-> 3
 
 
+int NumMassive = Readnum("Введите размер массива => ");
 
+int[] massive = FillArray();
+PrintArray(massive);
+delegate int input();
+input inp = new (Readnum);
+int Readnum(string massage)
+{
+    System.Console.Write(massage);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+
+int[] FillArray(int inp)
+{
+    int[] array = new int[NumMassive];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Readnum();
+    }
+    return array;
+}
+
+void PrintArray(int[] FillArray)
+{
+    for (int i = 0; i < massive.Length; i++)
+    {
+        System.Console.Write($"{massive[i]} ");
+    }
+}
--- task2/Program.cs
+++ task2/Program.cs
@@ -3,6 +3,18 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+float b1 = Readnum("b1");
+float k1 = Readnum("k1");
+float b2 = Readnum("b2");
+float k2 = Readnum("k2");
 
+float x = (b1 - b2) / (k2 - k1);
+float y = k2 * x + b2;
+System.Console.WriteLine($"x = {x}, y = {y}");
 
+int Readnum(string massage)
+{
+    System.Console.Write($"Введите {massage} => ");
+    return Convert.ToInt32(System.Console.ReadLine());
+}
 
